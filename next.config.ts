378430fcc3f512b38bfaea75b4619039dce0a48e
WenBin Lin <wblin2986@gmail.com>
2025-08-02 06:55:08 -0400

    fix github pages import path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const isLocal = process.env.NEXT_PUBLIC_BASE_PATH === '';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  ...(isProd && { 
+    output: 'export',
+    ...((!isLocal) && {
+      basePath: '/quick-apply-mosaic',
+      assetPrefix: '/quick-apply-mosaic/'
+    })
+  }),
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
